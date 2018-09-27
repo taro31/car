343996ffe7378dd743f39168d63a6a266c430e79
EC2 Default User <ec2-user@ip-172-31-35-230.ap-southeast-1.compute.internal>
2018-09-27 09:20:50 +0000

    bugfix

--- car.py
+++ car.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# -*- coding: utf-8 -*-
 from datetime import datetime, timedelta, timezone
 import requests
 import json
@@ -8,27 +8,27 @@ url = 'http://webservice.recruit.co.jp/carsensor/usedcar/v1/?key=' + apikey + '&
 
 response = requests.get(url)
 data = json.loads(response.text)
-num =  data["results"]["results_returned"]
+num = data["results"]["results_returned"]
 
 JST = timezone(timedelta(hours=+9), 'JST')
 now = datetime.now(JST).strftime("%Y-%m-%d %H:%M")
 
 f = open('car.txt', 'a')
-f.write('---------------' + now + '---------------\n' + num  + '台\n\n')
+f.write('---------------' + now + '---------------\n' + num + '台\n\n')
 f.close()
 
 num = int(num)
 
 for i in range(num):
-  model = data["results"]["usedcar"][i]["model"]
-  year = str(data["results"]["usedcar"][i]["year"])
-  color = data["results"]["usedcar"][i]["color"]
-  inspection = data["results"]["usedcar"][i]["inspection"]
-  odd = data["results"]["usedcar"][i]["odd"]
-  price = str(data["results"]["usedcar"][i]["price"])
+    model = data["results"]["usedcar"][i]["model"]
+    year = str(data["results"]["usedcar"][i]["year"])
+    color = data["results"]["usedcar"][i]["color"]
+    inspection = data["results"]["usedcar"][i]["inspection"]
+    odd = data["results"]["usedcar"][i]["odd"]
+    price = str(data["results"]["usedcar"][i]["price"])
 
-  f = open('car.txt', 'a')
-  f.write(model + '\n' + year + '\n' + color + '\n' + inspection + '\n' + odd + '\n' + price + '\n\n')
-  f.close()
+    f = open('car.txt', 'a')
+    f.write(model + '\n' + year + '\n' + color + '\n' + inspection + '\n' + odd + '\n' + price + '\n\n')
+    f.close()
 
 print('取得完了')
